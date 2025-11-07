9bf412bfe163ef049f3abdf31284e920275947e5
ermiHageez <ermihageez@gmail.com>
2025-11-07 13:14:18 +0000

    Add Django Debug Toolbar for development

--- backend/urls.py
+++ backend/urls.py
@@ -21,3 +21,8 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
